879a90ca55d0ef2500333de131db5875c2de333f
김선호 <kimseonho@gimseonhoui-MacBookPro.local>
2020-08-03 18:54:58 +0900

    weather app_expo

--- App.js
+++ App.js
@@ -13,8 +13,12 @@ export default class extends React.Component {
   };
 
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-    this.setState({ isLoading: false, temp: data.main.temp });
+    const {
+       data: {
+          main: { temp },
+          weather
+        } } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    this.setState({ isLoading: false, condition: weather[0].main, temp });
   };
 
   getLocation = async () => {
@@ -25,15 +29,15 @@ export default class extends React.Component {
     } catch (error) {
       Alert.alert("Can't find you", "no location.");
     }
-  };  
+  };
 
   componentDidMount() {
     this.getLocation();
   };
 
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
   }
 };
 
--- Weather.js
+++ Weather.js
@@ -1,26 +1,142 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+const weatherOptions = {
 
+    Thunderstorm: {
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"],
+        title: "Thunderstorm in the house",
+        subtitle: "    Made. seon ho"
+    },
+    Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle",
+        subtitle: "    Made. seon ho"
+    },
+    Rain: {
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"],
+        title: "Raining like a MF",
+        subtitle: "    Made. seon ho"
+    },
+    Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title: "Cold as balls",
+        subtitle: "    Made. seon ho"
+    },
+    Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        itle: "Astmosphere",
+        subtitle: "    Made. seon ho"
+    },
+    Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"],
+        title: "Sunny as fuck",
+        subtitle: "    Made. seon ho"
+    },
+    Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "    Made. seon ho"
+    },
+    Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Mist!",
+        subtitle: "    Made. seon ho"
+    },
+    Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Dusty",
+        subtitle: "    Made. seon ho"
+    },
+    Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Haze",
+        subtitle: "    Made. seon ho"
+    }
+};
 
-export default function Weather({ temp }) {
+export default function Weather({ temp, condition }) {
     return (
-    <View style={styles.container} >
-        <Text>{temp}</Text>
-    </View>
+        <LinearGradient
+            colors={weatherOptions[condition].gradient}
+            style={styles.container}
+        >
+            <StatusBar barStyle="light-content" />
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons
+                    size={96}
+                    name={weatherOptions[condition].iconName}
+                    color="white"
+                />
+                <Text style={styles.temp}>{temp}°</Text>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>
+                    {weatherOptions[condition].subtitle}
+                </Text>
+            </View>
+        </LinearGradient>
     );
-};
-
-
+}
 Weather.propTypes = {
-    temp: PropTypes.number.isRequired
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+    ]).isRequired
 };
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    temp: {
+        fontSize: 42,
+        color: "white"
+    },
+    halfContainer: {
+        flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    title: {
+        color: "white",
+        fontSize: 44,
+        fontWeight: "300",
+        marginBottom: 10,
+        textAlign: "left"
+    },
+    subtitle: {
+        fontWeight: "600",
+        color: "white",
+        fontSize: 24,
+        textAlign: "left"
+    },
+    textContainer: {
+        alignItems: "flex-start",
+        paddingHorizontal: 40,
+        justifyContent: "center",
+        flex: 1
     }
 });
